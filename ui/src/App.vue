0c5792ea63a4ec0bdb06515b01421b2bc3777932
Hannes B <boesch.hannes@gmail.com>
2019-05-22 00:23:23 +0200

    altered conference view

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Conference msg="Welcome to Your Vue.js App"/>
+  <div id="conference-app">
+      <div class ="topnav">
+          <a v-for="item in navheaders" v-bind:key="item.name" v-on:click="activeComponent=item.component">
+              {{ item.name }}
+          </a>
+      </div>
+      <div class="content">
+         <component v-bind:is="activeComponent"/>
+    </div>
   </div>
 </template>
 
@@ -12,6 +18,17 @@ export default {
   name: 'app',
   components: {
     Conference
+  },
+  data() {
+      return {
+        navheaders: [
+           {name : "Conferences", component : "Conference"},
+           {name: "Locations"},
+           {name: "Talks"},
+           {name: "Room-Flyer"} 
+        ],
+        activeComponent :  'Conference'
+      }
   }
 }
 </script>
@@ -25,4 +42,35 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+.content {
+  margin: 40px;
+
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
 </style>
--- ui/src/components/Conference.vue
+++ ui/src/components/Conference.vue
@@ -8,8 +8,9 @@
         <div v-if="loading">Loading...</div>
         <div v-else-if="error" class="error apollo">An error occured {{error}} </div>
         <ul v-else>
-          <li v-for="conference of data.Conference" class="conference">
-            {{ conference }}
+          <li v-for="conference of data.Conference" class="conference-list-item" v-bind:key="conference">
+            <span class="confnames">{{ conference.name }}</span><span>{{conference.startDate}} - {{conference.endDate}}</span>
+
           </li>
         </ul>
       </template>
@@ -39,54 +40,22 @@ export default {
 </script>
 
 <style scoped>
-.form,
-.input,
-.apollo,
-.message {
-  padding: 12px;
+ ul {
+  list-style-type: none;
+}
+.conference-list-item {
+  padding: 10px;
+  background-color: rgb(202, 197, 199);
+  width: 500px;
 }
 
-.conference {
-  display:block
-}
-
-label {
+.conference-list-item span {
   display: block;
-  margin-bottom: 6px;
 }
 
-.input {
-  font-family: inherit;
-  font-size: inherit;
-  border: solid 2px #ccc;
-  border-radius: 3px;
+.conference-list-item span:nth-child(2) {
+  color: blue;
+  font-size: x-small;
 }
 
-.error {
-  color: red;
-}
-
-.images {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, 300px);
-  grid-auto-rows: 300px;
-  grid-gap: 10px;
-}
-
-.image-item {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: #ccc;
-  border-radius: 8px;
-}
-
-.image {
-  max-width: 100%;
-  max-height: 100%;
-}
-
-.image-input {
-  margin: 20px;
-}
 </style>
