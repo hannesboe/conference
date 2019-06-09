53fe652c0caed71cd99cb7ae6e7b3c2194c8b7f1
Hannes B <boesch.hannes@gmail.com>
2019-06-10 01:57:23 +0200

    kubecon testdata for conference data file import, cleanup in api & deploy script

--- deploy/deploy-script.sh
+++ deploy/deploy-script.sh
@@ -24,16 +24,17 @@ fi
 
 #installing neo4j enterprise edition with 
 
-
 #set up docker context ()
 
-docker build -t conference_data:1 neo4j/initData/
+docker build -t conference-data:1 neo4j/initData/
 
 #todo set init container verion for plugins here
 helm install --name neo4j stable/neo4j --namespace persistence -f neo4j/neo4j.yaml 
 
-kubectl delete svc -n persistence neo4j-neo4j
-kubectl apply -f neo4j/neo-service-update.yaml
+# only needed to expose externally
+#kubectl delete svc -n persistence neo4j-neo4j
+#kubectl apply -f neo4j/neo-service-update.yaml
+
 # adding bolt port to service routing
 # kubectl patch service --namespace persistence neo4j-neo4j --patch "$(cat neo4j/service-port-patch.yaml)"
 # adding prometheus headers
@@ -59,8 +60,6 @@ kubectl set env deployment/ui VUE_APP_GRAPHQL_HTTP=http://$API_URL
 
 kubectl apply -f ui/service.yaml
 
-
-
 ## ingress deployment
 
 #minikube addons enable ingress
@@ -68,11 +67,13 @@ kubectl apply -f ui/service.yaml
 #helm install --name ingress stable/nginx-ingress --namespace ingress-nginx --set controller.metrics.enabled=true
 
 minikube addons enable ingress
-sed -e "s/\UI_URL/$UI_URL/" -e "s/\API_URL/$API_URL/" ingress/ingress.yaml | kubectl apply -f - -n ingress-nginx
+
+echo "using $UI_URL as ui and $API_URL as api"
+sed -e "s/\UI_URL/$UI_URL/" -e "s/\API_URL/$API_URL/" ingress/ingress.yaml | kubectl apply -f -
 
 
 # prometheus & grafana
 # https://github.com/kubernetes/ingress-nginx/blob/master/docs/user-guide/monitoring.md
 
-#kubectl apply --kustomize github.com/kubernetes/ingress-nginx/deploy/prometheus/
-#kubectl apply --kustomize github.com/kubernetes/ingress-nginx/deploy/grafana/
+kubectl apply --kustomize github.com/kubernetes/ingress-nginx/deploy/prometheus/
+kubectl apply --kustomize github.com/kubernetes/ingress-nginx/deploy/grafana/
